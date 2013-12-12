ac4221c41a1bd5900ace84fc820f78317a945d77
Robert Krzyzanowski <technoguyrob@gmail.com>
2013-12-12 14:26:14 -0600

    remove hardcoded s3 path

--- R/s3path.r
+++ R/s3path.r
@@ -1,5 +1,8 @@
 s3path <- function() {
-  # Hardcode for R conference
-  if (is.null(path <- getOption('s3mpi.path'))) path <- 's3://rconference/mpi'
+  if(is.null(path <- getOption('s3mpi.path'))) {
+    stop("Please set your s3 path using ",
+         "options(s3mpi.path = 's3://your_bucket/your/path/'). ",
+         "This is where all of your uploaded R objects will be stored.")
+  }
   path
 }
