7062e4c5896f8ac2b46e421b2c8102a72b9ceb02
RobertZK <technoguyrob@gmail.com>
2014-08-11 12:58:42 -0500

    close google connection check

--- R/utils.R
+++ R/utils.R
@@ -2,7 +2,8 @@ cache_enabled <- function() !is.null(tmp <- cache_directory()) && nzchar(tmp)
 cache_directory <- function() getOption('s3mpi.cache')
 
 has_internet <- function() {
-  internet_check <- tryCatch(warning = identity, file('http://google.com', 'r'))
+  internet_check <- tryCatch(warning = identity, try_google <- file('http://google.com', 'r'))
+  close(try_google)
   !(is(internet_check, 'warning') &&
     grepl('unable to resolve', internet_check$message))
 }
