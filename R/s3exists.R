f5aa35e769f58679b950bc30837adbf1e58f847d
Ryland Ely <ryland.ely@avantcredit.com>
2014-09-30 17:48:15 -0500

    s3exists

---
+++ R/s3exists.R
@@ -0,0 +1,12 @@
+#' Determine whether object exists on S3
+#' 
+#' Test whether or not the given object exists at the 
+#' give S3 path
+#'
+#' @export
+s3exists <- function(name, .path = s3path(), ...) { 
+  s3key <- paste(.path, name, sep = '')
+  s3cmd <- paste('s3cmd ls', s3key)
+  results <- system(s3cmd, intern=TRUE)
+  length(results) > 0
+}
