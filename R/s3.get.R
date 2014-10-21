59ea89f3af714ca43a754df629dd505c014740d7
RobertZK <technoguyrob@gmail.com>
2014-10-21 10:54:52 -0500

    hardcode s3cmd path

--- R/s3.get.R
+++ R/s3.get.R
@@ -1,7 +1,7 @@
 s3.get <- function (bucket, bucket.location = "US", verbose = FALSE, debug = FALSE) {
   AWS.tools:::check.bucket(bucket)
   x.serialized <- tempfile()
-  s3.cmd <- paste("s3cmd get", bucket, x.serialized, paste("--bucket-location",
+  s3.cmd <- paste("/usr/local/bin/s3cmd get", bucket, x.serialized, paste("--bucket-location",
       bucket.location), ifelse(verbose, "--verbose --progress",
       "--no-progress"), ifelse(debug, "--debug", ""))
   system(s3.cmd)
--- R/s3.put.R
+++ R/s3.put.R
@@ -4,7 +4,7 @@ s3.put <- function (x, bucket, bucket.location = "US", verbose = FALSE,
     AWS.tools:::check.bucket(bucket)
     x.serialized <- tempfile()
     saveRDS(x, x.serialized)
-    s3.cmd <- paste("s3cmd put", x.serialized, bucket, ifelse(encrypt,
+    s3.cmd <- paste("/usr/local/bin/s3cmd put", x.serialized, bucket, ifelse(encrypt,
         "--encrypt", ""), paste("--bucket-location", bucket.location),
         ifelse(verbose, "--verbose --progress", "--no-progress"), ifelse(debug,
             "--debug", ""), '--check-md5')
