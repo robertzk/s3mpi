c785f597f9111176dfe7b17b07d7c5fcd8aef18a
Ryland Ely <ryland.ely@avantcredit.com>
2014-10-28 14:18:49 -0500

    document args

--- R/s3exists.R
+++ R/s3exists.R
@@ -3,6 +3,9 @@
 #' Test whether or not the given object exists at the 
 #' give S3 path
 #'
+#' @param name string. Name of file to look for
+#' @param path string. Path to file
+
 #' @export
 s3exists <- function(name, .path = s3path(), ...) { 
   s3key <- paste(.path, name, sep = '')
