71bc033f56725603c0687f69a255d47ce78a1e38
Ryland Ely <ryland.ely@avantcredit.com>
2014-12-10 11:53:58 -0600

    strict matching only

--- R/s3exists.R
+++ R/s3exists.R
@@ -4,7 +4,7 @@
 #' give S3 path
 #'
 #' @param name string. Name of file to look for
-#' @param path string. Path to file
+#' @param path string. Path to file.  If missing, the entire s3 path must be provided in name.
 
 #' @export
 s3exists <- function(name, .path, ...) {
@@ -12,5 +12,5 @@ s3exists <- function(name, .path, ...) {
   if (!grepl('^s3://', s3key)) stop("s3 paths must begin with \"s3://\"")
   s3cmd <- paste('s3cmd ls', s3key)
   results <- system(s3cmd, intern=TRUE)
-  length(results) > 0
+  sum(grepl(pp('#{s3key}$'), results) > 0)
 }
