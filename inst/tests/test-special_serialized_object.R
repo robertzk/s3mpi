0c62def60e65fd95e62403d452bdfeff61a213aa
RobertZK <technoguyrob@gmail.com>
2014-12-10 14:31:45 -0600

    s3 path fix

--- inst/tests/test-special_serialized_object.R
+++ inst/tests/test-special_serialized_object.R
@@ -3,15 +3,18 @@ context('special serialized object')
 
 test_that('it can read special serialized object', {
   stub(s3read, "s3.get") <- function(...) structure(class =	'special_serialized_object',
-	                                                  list(deserialize = function(.) . + 1,
-													                               object = 1))
+	  list(deserialize = function(.) . + 1, object = 1))
+
   cached_value <- NULL
-  stub(s3read, "s3cache") <- function(...) if (length(list(...)) == 1) not_cached else cached_value <<- ..2
+  stub(s3read, "s3cache") <- function(...) {
+    if (length(list(...)) == 1) not_cached
+    else cached_value <<- ..2
+  }
 
   old_opts <- options(s3mpi.cache = tempdir())
   on.exit(options(old_opts))
 
-  expect_identical(s3read('blah'), 2)
+  expect_identical(s3read('blah', 'foo'), 2)
   expect_is(cached_value, 'special_serialized_object')
   expect_identical(deserialize(cached_value), 2)
 })
