17e4db948132ccbaeb58d8c477a0194edd64320d
Robert Krzyzanowski <technoguyrob@gmail.com>
2014-12-11 16:06:58 -0600

    fix this

--- R/s3exists.R
+++ R/s3exists.R
@@ -7,8 +7,8 @@
 #' @param path string. Path to file.  If missing, the entire s3 path must be provided in name.
 
 #' @export
-s3exists <- function(name, .path, ...) {
-  s3key <- if (missing(.path)) name else paste(.path, name, sep = '')
+s3exists <- function(name, .path = s3path(), ...) {
+  s3key <- paste(.path, name, sep = '')
   s3key <- gsub('/$', '', s3key) # strip terminal /
   if (!grepl('^s3://', s3key)) stop("s3 paths must begin with \"s3://\"")
   s3cmd <- paste('s3cmd ls', s3key)
