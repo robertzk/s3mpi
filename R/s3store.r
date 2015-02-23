0d771c2905d181de24046f0f8142d208daa795de
Kirill Sevastyanenko <kirill.sevastyanenko@avantcredit.com>
2015-02-23 16:31:14 -0600

    added safe option to s3store to prevent unintentional overwrites

--- R/s3store.r
+++ R/s3store.r
@@ -1,5 +1,5 @@
 #' Store an R object in S3 by key
-#' 
+#'
 #' Any type of object that can be serialized as an RDS file
 #' is capable of being retrieved using this interface.
 #'
@@ -9,10 +9,13 @@
 #' s3store(c(1,2,3), 'test123')
 #' print(s3read('test123'))
 #' # [1] 1 2 3
-#' }#' 
-s3store <- function(obj, name = NULL, .path = s3path(), ...) {
+#' }#'
+s3store <- function(obj, name = NULL, .path = s3path(), safe = TRUE, ...) {
   if (is.null(name)) name <- deparse(substitute(obj))
   s3key <- paste(.path, name, sep = '')
+  if (safe && s3mpi::s3exists(name, .path = s3path(), ...)) {
+    stop("An object with this name already exists. Use `safe = FALSE` to overwrite")
+  }
   obj4save <- s3normalize(obj, FALSE)
   s3mpi:::s3.put(obj4save, s3key, ...)
   if (!is.null(getOption('s3mpi.cache'))) s3cache(s3key, obj4save)
