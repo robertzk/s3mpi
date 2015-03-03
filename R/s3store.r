d654895d59c6d491eea64874840231124033cf33
RobertZK <technoguyrob@gmail.com>
2015-03-03 11:27:56 -0600

    remove unnecessary print

--- R/s3store.r
+++ R/s3store.r
@@ -21,7 +21,8 @@ s3store <- function(obj, name = NULL, .path = s3path(), safe = FALSE, ...) {
   s3key <- paste(.path, name, sep = '')
   if (isTRUE(safe) && s3exists(name, .path = .path, ...)) {
     # using cat prints to stdout as opposed to messages, so it can be seen from syberia::run_model()
-    cat(paste("An object with name", name, "on path", .path, "already exists. Use `safe = FALSE` to overwrite\n"))
+    cat("An object with name", name, "on path", .path,
+        "already exists. Use `safe = FALSE` to overwrite\n", sep = " ")
     stop("-------------------------^")
   }
   obj4save <- s3normalize(obj, FALSE)
