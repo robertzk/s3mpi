e451c9ef19b9444e158e42851fb63bf153e9ec28
Elaine K. Lee <elee1225@gmail.com>
2015-10-01 17:59:33 -0500

    Escape quotes, fix variable name collision for cache

--- R/s3.get.R
+++ R/s3.get.R
@@ -18,23 +18,23 @@ s3.get <- function (bucket, bucket.location = "US", verbose = FALSE, debug = FAL
 
   # Check for the bucket in the cache
   # If it does not exist, create and return its entry
-  if (!s3cache$exists(bucket)) {
+  if (!s3LRUcache$exists(bucket)) {
     ans <- fetch()
-    s3cache$set(bucket, ans)
+    s3LRUcache$set(bucket, ans)
   } else{
-    # Check time on s3cache's copy
-    last_cached <- s3cache$last_accessed(bucket) # assumes a POSIXct object
+    # Check time on s3LRUcache's copy
+    last_cached <- s3LRUcache$last_accessed(bucket) # assumes a POSIXct object
 
     # Check time on s3 remote's copy
-    s3.cmd <- paste("s3cmd ls ", bucket, "| awk '{print $1" "$2}' ")
+    s3.cmd <- paste("s3cmd ls ", bucket, "| awk '{print $1\" \"$2}' ")
     last_updated <- as.POSIXct(system(s3.cmd, intern = TRUE), tz="GMT")
 
     # Update the cache if remote is newer
     if (last_updated > last_cached) {
       ans <- fetch()
-      s3cache$set(bucket, ans)
+      s3LRUcache$set(bucket, ans)
     } else {
-      ans <- s3cache$get(bucket)
+      ans <- s3LRUcache$get(bucket)
     }
   }
   ans
