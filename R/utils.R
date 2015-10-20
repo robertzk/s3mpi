5ee335da504f159efec9d297a111a29d6fd3a556
RobertZK <technoguyrob@gmail.com>
2015-10-20 16:28:22 -0500

    Minor utils cleanup.

--- R/utils.R
+++ R/utils.R
@@ -35,9 +35,9 @@ cache_enabled <- function() {
 }
 
 cache_directory <- function() {
-  dir <- getOption('s3mpi.cache')
+  dir <- getOption("s3mpi.cache")
   if (!is.null(dir) && !(is.character(dir) && length(dir) == 1 && !is.na(dir))) {
-    stop("Please set the ", sQuote("s3mpi.cache"), " to a character ",
+    stop("Please set the ", sQuote("s3mpi.cache"), " option to a character ",
          "vector of length 1 giving a directory path.")
   }
   dir
@@ -46,15 +46,15 @@ cache_directory <- function() {
 has_internet <- local({
   has_internet_flag <- NULL
   function() {
-    if (!is.null(getOption('s3mpi.skip_connection_check'))) return(FALSE)
+    if (!is.null(getOption("s3mpi.skip_connection_check"))) return(FALSE)
     if (!is.null(has_internet_flag)) { return(has_internet_flag) }
     has_internet_flag <<- suppressWarnings({
-      internet_check <- try(file('http://google.com', 'r'))
-      if (!is(internet_check, 'try-error') && is(internet_check, 'connection')) {
+      internet_check <- try(file("http://google.com", "r"))
+      if (!is(internet_check, "try-error") && is(internet_check, "connection")) {
         on.exit(close.connection(internet_check))
       }
-      !(is(internet_check, 'try-error') &&
-        grepl('cannot open', internet_check$message))
+      !(is(internet_check, "try-error") &&
+        grepl("cannot open", internet_check$message))
     })
   }
 })
