75c6a0e7e8b62d6ff836622a1c2067cd4b9784fb
davidboren <david.boren@avant.com>
2016-05-06 13:47:16 -0700

    fix tests

--- tests/testthat/test-s3store.R
+++ tests/testthat/test-s3store.R
@@ -14,7 +14,7 @@ local({
   test_that("it can store raw values if the caching layer is disabled", {
     map <- list2env(list("s3://test/key" = NULL))
     testthatsomemore::package_stub("s3mpi", "s3.get",  function(...) map[[..1]], {
-    testthatsomemore::package_stub("s3mpi", "s3.put", function(...)  map[[..2]] <- ..1, {
+    testthatsomemore::package_stub("s3mpi", "s3.put", function(...)  map[[paste0(..2, ..3)]] <- ..1, {
       s3store("value", "key")
       expect_equal(s3read("key"), "value")
       map$`s3://test/key` <- "new_value"
@@ -27,7 +27,7 @@ local({
     map <- list2env(list("s3://test/key" = NULL))
     map2 <- new.env(parent = map)
     testthatsomemore::package_stub("s3mpi", "s3.get",  function(...) map2[[..1]], {
-    testthatsomemore::package_stub("s3mpi", "s3.put", function(...) map2[[..2]] <- ..1, {
+    testthatsomemore::package_stub("s3mpi", "s3.put", function(...)  map[[paste0(..2, ..3)]] <- ..1, {
       s3store("value", "key")
       expect_equal(s3read("key"), "value")
       map$`s3://test/key` <- "new_value"
@@ -41,7 +41,7 @@ local({
 
     testthatsomemore::package_stub("s3mpi", "s3normalize",  function(a, b) { map$norm <- missing(b); a }, {
     testthatsomemore::package_stub("s3mpi", "s3.get",  function(...) map[[..1]], {
-    testthatsomemore::package_stub("s3mpi", "s3.put", function(...)  map[[..2]] <- ..1, {
+    testthatsomemore::package_stub("s3mpi", "s3.put", function(...)  map[[paste0(..2, ..3)]] <- ..1, {
       s3store("value", "key")
       expect_false(map$norm)
       s3store(new.env(), "key2")
@@ -52,7 +52,7 @@ local({
   test_that("it can pick up missing key", {
     map <- list2env(list("s3://test/key" = NULL))
     testthatsomemore::package_stub("s3mpi", "s3.get",  function(...) map[[..1]], {
-    testthatsomemore::package_stub("s3mpi", "s3.put", function(...)  map[[..2]] <- ..1, {
+    testthatsomemore::package_stub("s3mpi", "s3.put", function(...)  map[[paste0(..2, ..3)]] <- ..1, {
       key <- "value"
       s3store(key)
       expect_equal(s3read("key"), "value")
