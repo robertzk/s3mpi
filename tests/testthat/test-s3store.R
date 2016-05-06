dc6579bcaa3a4554aa764876b734b3c80c1d06c0
davidboren <david.boren@avant.com>
2016-05-06 14:16:19 -0700

    Mock exists

--- tests/testthat/test-s3store.R
+++ tests/testthat/test-s3store.R
@@ -13,6 +13,7 @@ local({
 
   test_that("it can store raw values if the caching layer is disabled", {
     map <- list2env(list("s3://test/key" = NULL))
+    testthatsomemore::package_stub("s3mpi", "s3exists",  function(...) TRUE, {
     testthatsomemore::package_stub("s3mpi", "s3.get",  function(...) map[[..1]], {
     testthatsomemore::package_stub("s3mpi", "s3.put", function(...)  map[[paste0(..2, ..3)]] <- ..1, {
       s3store("value", "key")
@@ -20,12 +21,13 @@ local({
       map$`s3://test/key` <- "new_value"
       # Make sure we are not caching.
       expect_equal(s3read("key", cache = FALSE), "new_value")
-    })})
+    })})})
   })
 
   test_that("it can store values if the caching layer is enabled", {
     map <- list2env(list("s3://test/key" = NULL))
     map2 <- new.env(parent = map)
+    testthatsomemore::package_stub("s3mpi", "s3exists",  function(...) TRUE, {
     testthatsomemore::package_stub("s3mpi", "s3.get",  function(...) map2[[..1]], {
     testthatsomemore::package_stub("s3mpi", "s3.put", function(...)  map2[[paste0(..2, ..3)]] <- ..1, {
       s3store("value", "key")
@@ -33,12 +35,13 @@ local({
       map$`s3://test/key` <- "new_value"
       # Make sure we are not caching.
       expect_equal(s3read("key"), "value")
-    })})
+    })})})
   })
 
   test_that("it denormalizes", {
     map <- list2env(list("s3://test/key" = "value"))
 
+    testthatsomemore::package_stub("s3mpi", "s3exists",  function(...) TRUE, {
     testthatsomemore::package_stub("s3mpi", "s3normalize",  function(a, b) { map$norm <- missing(b); a }, {
     testthatsomemore::package_stub("s3mpi", "s3.get",  function(...) map[[..1]], {
     testthatsomemore::package_stub("s3mpi", "s3.put", function(...)  map[[paste0(..2, ..3)]] <- ..1, {
@@ -46,17 +49,35 @@ local({
       expect_false(map$norm)
       s3store(new.env(), "key2")
       expect_true(map$norm)
-    })})})
+    })})})})
   })
 
   test_that("it can pick up missing key", {
     map <- list2env(list("s3://test/key" = NULL))
+    testthatsomemore::package_stub("s3mpi", "s3exists",  function(...) TRUE, {
     testthatsomemore::package_stub("s3mpi", "s3.get",  function(...) map[[..1]], {
     testthatsomemore::package_stub("s3mpi", "s3.put", function(...)  map[[paste0(..2, ..3)]] <- ..1, {
       key <- "value"
       s3store(key)
       expect_equal(s3read("key"), "value")
-    })})
+    })})})
+  })
+
+  test_that("it produces an error when the object isn't found with an s3exists following the s3.put", {
+    map <- list2env(list("s3://test/key" = NULL))
+    testthatsomemore::package_stub("s3mpi", "s3exists",  function(...) FALSE, {
+    testthatsomemore::package_stub("s3mpi", "s3.get",  function(...) map[[..1]], {
+    testthatsomemore::package_stub("s3mpi", "s3.put", function(...)  map[[paste0(..2, ..3)]] <- ..1, {
+      expect_error(s3store("value", "key"))
+    })})})
+  })
+  test_that("it does not produce an error when the object is found with an s3exists following the s3.put", {
+    map <- list2env(list("s3://test/key" = NULL))
+    testthatsomemore::package_stub("s3mpi", "s3exists",  function(...) TRUE, {
+    testthatsomemore::package_stub("s3mpi", "s3.get",  function(...) map[[..1]], {
+    testthatsomemore::package_stub("s3mpi", "s3.put", function(...)  map[[paste0(..2, ..3)]] <- ..1, {
+      expect_error(s3store("value", "key"), NA)
+    })})})
   })
 })
 
