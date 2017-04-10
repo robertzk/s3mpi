f97c20e99a22efa8b7dc142ff97680bdf59c9cee
abelcastilloavant <abel.castillo@avant.com>
2017-04-10 10:00:04 -0500

    fix typo

--- R/s3.put.R
+++ R/s3.put.R
@@ -44,7 +44,7 @@ s3.put <- function (x, path, name, bucket_location = "US",
   save_to_file <- get(paste0("save_as_", storage_format))
   save_to_file(x, x.serialized, row.names, ...)
 
-  cmd <- s3cmd_put_command(s3key, x.serialized, bucket_location_to_flag(bucket_location debug)
+  cmd <- s3cmd_put_command(s3key, x.serialized, bucket_location_to_flag(bucket_location), debug)
   run_system_put(path, name, cmd, check_exists, num_retries, backoff)
 }
 
